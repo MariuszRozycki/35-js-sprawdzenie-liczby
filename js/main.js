e262c3d4e6e72abfeb8f13499a1d8bdb65240b1b
mariansc83 <mariansc83@gmail.com>
2020-09-28 17:18:34 +0200

    Add semicolon after console.log

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function number(n) {
   if (n >= 100 && n <= 200) {
-    console.log(`Liczba znajduje sie w przedziale 100 - 200`)
+    console.log(`Liczba znajduje sie w przedziale 100 - 200`);
   } else {
-    console.log(`Liczba nie znajduje sie w przedziale 100 - 200`)
+    console.log(`Liczba nie znajduje sie w przedziale 100 - 200`);
   }
 }
 
